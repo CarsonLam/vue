4aa37c929010a2313e9c239be5d43c1335c2d20d
carson <310431021@qq.com>
2020-05-20 22:07:18 +0800

    11
    111

---
+++ test/index.vue
@@ -0,0 +1,12 @@
+<template> </template>
+<script>
+export default {
+  data() {
+    return {
+      constants: {},
+    };
+  },
+  methods: {},
+};
+</script>
+<style src="./index.response.css" />
